0941a30481989d3150a2297ee61f6ef3a64085fa
Arif A Mondal <arifali.mondal@gmail.com>
2023-10-26 01:17:02 +0300

    add multiple files processing

---
+++ src/main/java/com/example/app/service/FileMonitoringService.java
@@ -0,0 +1,43 @@
+package com.example.app.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class FileMonitoringService {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileMonitoringService.class);
+
+    public static File getFile(String sourceDirectory) {
+        logger.info("Directory Name " + sourceDirectory);
+        File sourceDir = new File(sourceDirectory);
+
+        if (sourceDir.exists() && sourceDir.isDirectory()) {
+            File[] files = sourceDir.listFiles();
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".csv")) {
+                    return file;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void moveFile(File source, File destination) {
+        try {
+            Path sourcePath = source.toPath();
+            Path destinationPath = destination.toPath();
+
+            Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+            logger.info("File moved {} successfully", destination.getName());
+        } catch (IOException e) {
+            logger.error("Failed to move the file: {}", e.getMessage());
+        }
+    }
+}
