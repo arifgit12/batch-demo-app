75721b6ab05df5abc5a35c8520834a945541dd27
Arif A Mondal <arifali.mondal@gmail.com>
2023-10-25 08:20:38 +0300

    add employee interface

--- src/main/java/com/example/app/service/EmployeeService.java
+++ src/main/java/com/example/app/service/EmployeeService.java
@@ -1,40 +1,20 @@
 package com.example.app.service;
 
 import com.example.app.entity.Employee;
-import com.example.app.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional(readOnly = true)
-public class EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+public interface EmployeeService {
 
-    @Transactional(readOnly = false)
-    public Employee createEmployee(Employee employee) {
-        return employeeRepository.save(employee);
-    }
+    Employee createEmployee(Employee employee);
 
-    public Employee getEmployeeById(Long id) {
-        return employeeRepository.findById(id).orElse(null);
-    }
+    Employee getEmployeeById(Long id);
 
-    public Page<Employee> getAllEmployees(Pageable pageable) {
-        return employeeRepository.findAll(pageable);
-    }
+    Page<Employee> getAllEmployees(Pageable pageable);
 
-    @Transactional(readOnly = false)
-    public void updateEmployee(Employee employee) {
-        employeeRepository.save(employee);
-    }
+    void updateEmployee(Employee employee);
 
-    public void deleteEmployee(Long id) {
-        employeeRepository.deleteById(id);
-    }
+    void deleteEmployee(Long id);
 }
 
