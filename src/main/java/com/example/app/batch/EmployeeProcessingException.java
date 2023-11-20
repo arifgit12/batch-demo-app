d9a0283b286202b82124dd52dd1d15efd788b655
Arif A Mondal <arifali.mondal@gmail.com>
2023-11-21 00:21:12 +0300

    add EmployeeProcessingException

---
+++ src/main/java/com/example/app/batch/EmployeeProcessingException.java
@@ -0,0 +1,5 @@
+package com.example.app.batch;
+
+public class EmployeeProcessingException extends Exception {
+
+}
--- src/main/java/com/example/app/batch/EmployeeProcessor.java
+++ src/main/java/com/example/app/batch/EmployeeProcessor.java
@@ -7,6 +7,16 @@ public class EmployeeProcessor implements ItemProcessor<Employee, Employee> {
 
     @Override
     public Employee process(Employee customer) throws Exception {
+        Employee filterCustomer = getValidCustomer(customer);
+        return filterCustomer;
+    }
+
+    private Employee getValidCustomer(Employee customer) throws EmployeeProcessingException {
+
+        if(Math.random() < .03) {
+            throw new EmployeeProcessingException();
+        }
+
         return customer;
     }
 }
